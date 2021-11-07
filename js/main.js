a1f07dbe902cbad5cb628743edf45c970f9f1077
Amanjot726 <67585828+Amanjot726@users.noreply.github.com>
2021-11-07 18:50:12 +0530

    Footer
    By Amanjot Singh

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 $(document).ready(function (e) {
-    $(document).bind("contextmenu",function(e) {
-      return false;
+  var CurrentYear = new Date().getFullYear();
+  document.getElementById('date').innerText = CurrentYear;
+
+  $(document).bind("contextmenu",function(e) {
+    return false;
   });
 });
 
@@ -11,10 +14,49 @@ function Fid(idv){if(document.getElementById(idv)===null){throw "Error:\n  Unabl
 //   document.getElementsByClassName
 // }
 
+
+
+// Navbar-controller
 Fid('menu-btn').onclick=function(){
   Fid('menu-btn').classList.toggle("change");
   if (Fid('nav-links').classList.contains("nav-open")){Fid('nav-links').classList.add("nav-close");Fid('nav-links').classList.remove("nav-open");}
   else{Fid('nav-links').classList.add("nav-open");Fid('nav-links').classList.remove("nav-close");}
 };
+// End of Navbar-controller
+
+
+
+
+// Scroll to top button
+const scrollToTopButton = document.getElementById('js-top');
+const scrollFunc = () => {
+    let y = window.scrollY;
+    if (y > 700) { scrollToTopButton.className = "btn btn-dark show"; }
+    else { scrollToTopButton.className = "btn btn-dark hide"; }
+};
+window.addEventListener("scroll", scrollFunc);
+var pre = document.documentElement.scrollTop || document.body.scrollTop;
+const scrollToTop = () => {
+    const c = document.documentElement.scrollTop || document.body.scrollTop;
+    if (c > 0) {
+        if(pre<c){
+            return;
+        }
+        else{
+            pre = c;
+            window.requestAnimationFrame(scrollToTop);
+            window.scrollTo(0, c - c / 25);
+        }
+    }
+};
+scrollToTopButton.onclick = function (e) {
+    e.preventDefault();
+    pre = document.documentElement.scrollTop || document.body.scrollTop;
+    scrollToTop();
+}
+// END Scroll to top button
+
+
+
 
 // document.getElementById("").style.display
