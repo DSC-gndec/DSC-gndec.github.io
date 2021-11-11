c95945017a294d867ace6bf2a6f67fdd12bf11f2
Amanjot726 <67585828+Amanjot726@users.noreply.github.com>
2021-11-12 02:07:20 +0530

    Improvements
    By Amanjot Singh

--- js/main.js
+++ js/main.js
@@ -60,27 +60,102 @@ scrollToTopButton.onclick = function (e) {
 
 function snackbar(Notification) {var x = document.getElementById("snackbar");x.innerHTML=Notification;x.className = "show";setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);}
 
+
+// ========================= /Start-subscribe-form/=================================
 var form = document.getElementById("my-form");
-    
-    async function handleSubmit(event) {
-      event.preventDefault();
-      var status = document.getElementById("my-form-status");
-      var data = new FormData(event.target);
-      fetch(event.target.action, {
-        method: form.method,
-        body: data,
-        headers: {
-            'Accept': 'application/json'
-        }
-      }).then(response => {
+async function handleSubscribe(event) {
+  event.preventDefault();
+  var data = new FormData(event.target);
+  if (data.get("email") == ""){
+    document.querySelector('.f-subscribe').setAttribute('required', '');
+    snackbar("Please fill the Email");
+    return;
+  }
+  else{
+    fetch(event.target.action, {
+      method: form.method,
+      body: data,
+      headers: {
+          'Accept': 'application/json'
+      }
+    }).then(response => {
+      if (response.ok) {
         snackbar("Thanks for subscribing!");
         form.reset()
-      }).catch(error => {
-        snackbar("Oops! Facing some issues , try again later");
-      });
-    }
-    form.addEventListener("submit", handleSubmit);
+      } else {
+        snackbar("Oops! Facing some issues<br>Please try again later");
+      }
+    }).catch(error => {
+      snackbar("Oops! Facing some issues<br>Please try again later");
+    });
+  }
+}
+form.addEventListener("submit", handleSubscribe);
+// ========================= /End-subscribe-form/=================================
 
+
+
+// ========================= /Start-Modal/=================================
+var enlargable_elements = document.querySelectorAll('img[data-enlarge-available="true"]');
+enlargable_elements.forEach(element => {
+  element.addEventListener('click', function(e) {
+    try{
+      console.log("here");
+      document.querySelector('#modal-body>img').setAttribute('src', element.getAttribute('src'));
+      document.querySelector('#modal-body>img').setAttribute('alt', element.getAttribute('alt'));
+      document.querySelector('#modal-body>img').setAttribute('alt', element.getAttribute('alt'));
+      document.querySelector('#modal-title').innerHTML = element.getAttribute('alt')+" EVENT";
+      $('#ImageModal').modal('show');
+    }
+    catch(err){}
+  });
+});
+// ========================= /End-Modal/=================================
+
+
+
+// ========================= /Start-Contact-form/=================================
+var form = document.getElementById("contact-form");
+async function handleContactForm(event) {
+  event.preventDefault();
+  var data = new FormData(event.target);
+  if (data.get("name") == ""){
+    document.querySelector('#name').setAttribute('required', '');
+    snackbar("Please fill your name");
+    return;
+  }
+  if (data.get("email") == ""){
+    document.querySelector('#email').setAttribute('required', '');
+    snackbar("Please fill your email");
+    return;
+  }
+  if (data.get("message") == ""){
+    document.querySelector('#message').setAttribute('required', '');
+    snackbar("Please fill type something in message");
+    return;
+  }
+  else{
+    fetch(event.target.action, {
+      method: form.method,
+      body: data,
+      headers: {
+          'Accept': 'application/json'
+      }
+    }).then(response => {
+      if (response.ok) {
+        snackbar("Thanks for giving your details<br>We will catch you soon.."); 
+        form.reset();
+      } else {
+        snackbar("Oops! Facing some issues<br>Please try again later");
+      }
+    }).catch(error => {
+      snackbar("Oops! Facing some issues<br>Please try again later");
+    });
+  }
+}
+form.addEventListener("submit", handleContactForm);
+
+// ========================= /End-Contact-form/=================================
 
 
 // document.getElementById("").style.display
